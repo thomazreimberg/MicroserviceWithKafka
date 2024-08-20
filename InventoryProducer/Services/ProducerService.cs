0a8b8462e1d557c381d504f1c36dfe5e20e77789
Thomaz Henrique <treimberg7@gmail.com>
2024-08-20 20:27:57 -0300

    Add project files.

---
+++ InventoryProducer/Services/ProducerService.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+
+namespace InventoryProducer.Services
+{
+    public class ProducerService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IProducer<Null, string> _producer;
+
+        public ProducerService(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = _configuration["Kafka:BootstrapServers"]
+            };
+
+            _producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+        }
+
+        public async Task ProduceAsync(string topic, string message)
+        {
+            var kafkamessage = new Message<Null, string> { Value = message, };
+
+            await _producer.ProduceAsync(topic, kafkamessage);
+        }
+    }
+}
